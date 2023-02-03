5ffb7eda7cbd020fcef15234e73bbb4e0154af4b
Md Irfan <82992328+mdirfancse2023@users.noreply.github.com>
2023-02-03 14:10:53 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import Navbar from "./components/Navbar"
 import TextForm from "./components/TextForm"
 import Footer from "./components/Footer"
 import "./App.css";
+import About from "./components/About";
+import { useState } from "react";
 function App() {
+  const [mode,setMode] = useState('light');
+  const [tmode,tsetMode] = useState('dark');
+  const toggle = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      tsetMode('light');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      tsetMode('dark');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" />
-      <TextForm/>
-      <Footer/>
+      <Navbar title="TextUtils" mode={mode} tmode={tmode} toggle={toggle}/>
+      <TextForm mode={mode} tmode={tmode}/>
+      <Footer mode={mode} tmode={tmode}/>
+      {/* <About/> */}
     </>
   );
 }
